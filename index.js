a4bebe7f5f285bed123216c8409a28f1c80a00a8
Svarogichz <87203267+Svarogichz@users.noreply.github.com>
2025-05-27 17:54:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('I knew u would say yes >:3   ')
+    alert('I knew u would say yes   ')
 });
 
 const noBtn = document.querySelector('#noBtn');
